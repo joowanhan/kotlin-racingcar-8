348cf8fc409fb3f4967dc1084f875378d1cbb8b6
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-05 18:38:47 +0900

    feat(car): add input of car names and attempts

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+	// TODO: 프로그램 구현
+	val names = camp.nextstep.edu.missionutils.Console.readLine().split(",")
+	val attempts = camp.nextstep.edu.missionutils.Console.readLine().toIntOrNull()
+		?: throw IllegalArgumentException("Invalid number format")
+	
+	val cars = names.map { Car(it) }
+	
 }

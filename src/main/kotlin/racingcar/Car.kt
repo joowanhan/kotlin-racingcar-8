13f11745d7c612e4e15457d2ecbeb6c7fa7fb6bf
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-05 17:39:13 +0900

    feat(car): add car class
     - move method

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+const val threshold = 4
+
+class Car(name: String) {
+	
+	var step = 0
+	fun move() {
+		val point = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9)
+		if (point >= threshold) {
+			++step
+		}
+		
+	}
+}

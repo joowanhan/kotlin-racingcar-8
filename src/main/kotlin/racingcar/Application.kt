f64d5e3d17008f0414b09d88cd461798700b824f
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-06 16:06:18 +0900

    feat(application): add print message for result

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,7 @@ fun main() {
 	// TODO: 프로그램 구현
 	val nameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 	val attemptsMessage = "시도할 횟수는 몇 회인가요?"
-	
+	val resultMessage = "실행결과"
 	println(nameMessage)
 	val names = camp.nextstep.edu.missionutils.Console.readLine().split(",")
 	
@@ -14,6 +14,7 @@ fun main() {
 	
 	val cars = names.map { Car(it) }
 	
+	println(resultMessage)
 	for (i in 1 .. attempts) {
 		cars.onEach { it.move() }.forEach { println("${it.name} : ${it.printStep()}") }
 		

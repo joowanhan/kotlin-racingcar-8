c07a925a4cb575bf493fde604f399a567072d868
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-06 15:29:48 +0900

    refactor(car): change minor issues
    - constructor parameter(name) to class property to external access
    - optimize import

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,13 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 const val threshold = 4
 
-class Car(name: String) {
+class Car(val name: String) {
 	
 	var step = 0
 	
 	fun move() {
-		val point = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9)
+		val point = Randoms.pickNumberInRange(0, 9)
 		if (point >= threshold) {
 			++step
 		}

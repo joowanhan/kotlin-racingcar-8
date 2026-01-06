f65ad2dd61b18d0617ab856b7e0f605793d90007
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-06 23:57:09 +0900

    feat(application): add input validation for car names and attempts

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,13 +7,18 @@ fun main() {
 	val nameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 	val attemptsMessage = "시도할 횟수는 몇 회인가요?"
 	val resultMessage = "실행결과"
+	
 	println(nameMessage)
 	val names = readLine().split(",")
+	require(names.isNotEmpty()) { "이름이 빈칸으로 입력될 수 없습니다." }
+	names.forEach { require(it.length <= 5) { "이름이 5자 초과입니다. (\"$it\" 등)" } }
 	
 	println(attemptsMessage)
 	val attempts = readLine().toIntOrNull() ?: throw IllegalArgumentException("Invalid number format")
+	require(attempts > 0) { "시도 횟수는 1 이상이어야 합니다." }
 	
 	val cars = names.map { Car(it) }
+	
 	
 	println(resultMessage)
 	for (i in 1 .. attempts) {

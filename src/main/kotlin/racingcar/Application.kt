670704f0205e798d60d79cdacbfa98a4ad6488cc
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-01-06 23:39:24 +0900

    refactor(application): optimize import

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,16 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.*
+
 fun main() {
 	// TODO: 프로그램 구현
 	val nameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 	val attemptsMessage = "시도할 횟수는 몇 회인가요?"
 	val resultMessage = "실행결과"
 	println(nameMessage)
-	val names = camp.nextstep.edu.missionutils.Console.readLine().split(",")
+	val names = readLine().split(",")
 	
 	println(attemptsMessage)
-	val attempts = camp.nextstep.edu.missionutils.Console.readLine().toIntOrNull()
-		?: throw IllegalArgumentException("Invalid number format")
+	val attempts = readLine().toIntOrNull() ?: throw IllegalArgumentException("Invalid number format")
 	
 	val cars = names.map { Car(it) }
 	
